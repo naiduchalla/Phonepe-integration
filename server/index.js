b931b37cd037cad1ba5308f0486c931e3c515e68
unknown <naiduch1981@gmail.com>
2024-06-13 13:37:12 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,133 @@
+const express = require("express");
+const cors = require("cors");
+const crypto = require("crypto");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const port= 8000;
+
+
+require("dotenv").config();
+
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(cors());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+
+let salt_key = '099eb0cd-02cf-4e2a-8aca-3e6c6aff0399'
+let merchant_id = 'PGTESTPAYUAT'
+
+app.get("/", (req, res) => {
+    res.send("server is running");
+})
+
+
+app.post("/order", async (req, res) => {
+
+    try {
+        console.log(req.body)
+
+        const merchantTransactionId = req.body.transactionId;
+        const data = {
+            merchantId: merchant_id,
+            merchantTransactionId: merchantTransactionId,
+            merchantUserId: req.body.MUID,
+            name: req.body.name,
+            amount: req.body.amount * 100,
+            redirectUrl: `http://localhost:8000/status/?id=${merchantTransactionId}`,
+            redirectMode: 'POST',
+            mobileNumber: req.body.number,
+            paymentInstrument: {
+                type: 'PAY_PAGE'
+            }
+        };
+        const payload = JSON.stringify(data);
+        const payloadMain = Buffer.from(payload).toString('base64');
+        const keyIndex = 1;
+        const string = payloadMain + '/pg/v1/pay' + salt_key;
+        const sha256 = crypto.createHash('sha256').update(string).digest('hex');
+        const checksum = sha256 + '###' + keyIndex;
+
+        // const prod_URL = "https://api.phonepe.com/apis/hermes/pg/v1/pay"
+        const prod_URL = "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay"
+
+        const options = {
+            method: 'POST',
+            url: prod_URL,
+            headers: {
+                accept: 'application/json',
+                'Content-Type': 'application/json',
+                'X-VERIFY': checksum
+            },
+            data: {
+                request: payloadMain
+            }
+        };
+
+        await axios(options).then(function (response) {
+                console.log(response.data)
+
+                return res.json(response.data)
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+
+    } catch (error) {
+        res.status(500).send({
+            message: error.message,
+            success: false
+        })
+    }
+
+})
+
+
+app.post("/status", async (req, res) => {
+
+    const merchantTransactionId = req.query.id
+    const merchantId = merchant_id
+
+    const keyIndex = 1;
+    const string = `/pg/v1/status/${merchantId}/${merchantTransactionId}` + salt_key;
+    const sha256 = crypto.createHash('sha256').update(string).digest('hex');
+    const checksum = sha256 + "###" + keyIndex;
+
+    const options = {
+        method: 'GET',
+        url: `https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/status/${merchantId}/${merchantTransactionId}`,
+        headers: {
+            accept: 'application/json',
+            'Content-Type': 'application/json',
+            'X-VERIFY': checksum,
+            'X-MERCHANT-ID': `${merchantId}`
+        }
+    };
+
+    // CHECK PAYMENT TATUS
+    axios.request(options).then(async (response) => {
+            if (response.data.success === true) {
+                const url = `http://localhost:5173/success`
+                return res.redirect(url)
+            } else {
+                const url = `http://localhost:5173/failure`
+                return res.redirect(url)
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
